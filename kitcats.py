38e74c246899701492364983f52ca6c91ef0b7c2
Bxrtxrr <67070277@kmitl.ac.th>
2024-10-13 15:34:34 +0700

    Responsive Background

--- kitcats.py
+++ kitcats.py
@@ -1,7 +1,13 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
+
+background = pygame.image.load("Menu.jpg")
+
+def update_background(size):
+    return pygame.transform.scale(background, size)
 
 pygame.display.set_caption("KITCATS")
 icons = pygame.image.load("geometry.png")
@@ -9,10 +15,14 @@ pygame.display.set_icon(icons)
 
 running = True
 while running:
-    screen.fill((0, 0, 0))
+    screen.blit(background, (0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.VIDEORESIZE:
+            width, height = event.w, event.h
+            screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
+        background = update_background((width, height))
 
     # player()
     pygame.display.update()
